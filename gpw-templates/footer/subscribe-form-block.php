4f063ead33cff8cc6bdbc1123ad040d6666e2a27
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2025-11-26 16:38:00 +0700

    UPDATE: add script to make block popup a bit over footer

--- gpw-templates/footer/subscribe-form-block.php
+++ gpw-templates/footer/subscribe-form-block.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * @author Hieu "JIN" Phan Trung
  * * Template: Footer - Subscribe Form Block
@@ -8,20 +8,23 @@ $formSC = '[contact-form-7 id="766d840" title="FOOTER: Subcriber form"]';
 ?>
 <div class="footer__subscribe-block subscribe-block">
   <div class="subscribe-block__content">
-    <?php if( !empty( $subscribeFormData['sub_title'] ) ): ?>
-      <span class="subscribe-block__sub-title"><?= esc_html( $subscribeFormData['sub_title'] ) ?></span>
+    <?php if (!empty($subscribeFormData['sub_title'])): ?>
+      <span class="subscribe-block__sub-title"><?= esc_html($subscribeFormData['sub_title']) ?></span>
     <?php endif; ?>
-    <?php if( !empty( $subscribeFormData['title'] ) ): ?>
-      <h2 class="subscribe-block__title"><?= wp_kses_post( $subscribeFormData['title'] ) ?></h2>
+    <?php if (!empty($subscribeFormData['title'])): ?>
+      <h2 class="subscribe-block__title"><?= wp_kses_post($subscribeFormData['title']) ?></h2>
     <?php endif; ?>
-    <?php if( !empty( $subscribeFormData['description'] ) ): ?>
-      <div class="subscribe-block__desc"><?= wp_kses_post( $subscribeFormData['description'] ) ?></div>
+    <?php if (!empty($subscribeFormData['description'])): ?>
+      <div class="subscribe-block__desc"><?= wp_kses_post($subscribeFormData['description']) ?></div>
     <?php endif; ?>
   </div>
   <div class="subscribe-block__form">
-    <?= do_shortcode( $formSC ) ?>
+    <?= do_shortcode($formSC) ?>
   </div>
 </div>
+<script>
+  jQuery(document).ready(function($){$('.footer__subscribe-block').css('margin-top',function(){return $(this).outerHeight()/2*-1+'px'})});
+</script>
 <?php
 // ! Clean up variables
-unset( $subscribeFormData, $formSC );
+unset($subscribeFormData, $formSC);
