61a753331c0e8e70627708a770d93ccd6d2657a8
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2025-11-26 16:39:04 +0700

    UPDATE: facebook and google embed

--- inc/controller/CompanyInfo.php
+++ inc/controller/CompanyInfo.php
@@ -10,6 +10,8 @@ class CompanyInfo {
   private string $email;
   private string $phone;
   private array $socials;
+  private string $facebookEmbed;
+  private string $mainOfficeMap;
   public static function getInstance(): CompanyInfo {
     if( !isset( self::$instance ) ) {
       self::$instance = new CompanyInfo();
@@ -26,11 +28,29 @@ class CompanyInfo {
     $this->email = $companyInfo['email'] ?? '';
     $this->phone = $companyInfo['phone_number'] ?? '';
     $this->socials = $companyInfo['social'] ?? [];
+    $this->facebookEmbed = $companyInfo['facebook_embed'] ?? '';
+    $this->mainOfficeMap = $companyInfo['main_office_map_embed'] ?? '';
   }
   public function getOffice(): array {
     return $this->office;
   }
   public function getPhoneNumber(): string {
     return $this->phone;
+  }
+  public function getEmail(): string {
+    return $this->email;
+  }
+  public function getSocials(): array {
+    return $this->socials;
+  }
+  public function getFacebookEmbed(): string {
+    return $this->facebookEmbed;
+  }
+  public function getMainOfficeMap(): string {
+    return $this->mainOfficeMap;
+  }
+  public function getDefaultLogo() {
+    $logoID = get_theme_mod( 'site_logo', get_template_directory_uri() . '/assets/img/logo.png' );
+    return $logoID;
   }
 }
