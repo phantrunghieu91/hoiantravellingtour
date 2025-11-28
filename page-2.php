e129914268c7e951d3fcb1b139e61ee617dcca7e
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2025-11-28 09:01:48 +0700

    CREATE: home page template, hero section template + style for hero section

---
+++ page-2.php
@@ -0,0 +1,11 @@
+<?php 
+/**
+ * @author Hieu "Jin" Phan Trung
+ * * Template: Home page
+ */
+
+get_template_part( 'gpw-templates/global/header' );
+
+get_template_part( 'gpw-templates/global/hero-section', 'with-content' );
+
+get_template_part( 'gpw-templates/global/footer' );
