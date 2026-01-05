fd2411095dc408308ad7916afcb7c9ad786cd1b1
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-01-05 16:35:04 +0700

    UPDATE: added template render with default value, hidden css

--- gpw-templates/post/post-card.php
+++ gpw-templates/post/post-card.php
@@ -17,21 +17,20 @@ $isFeatured = $args['is_featured'] ?? false;
 $footerDisplay = $args['footer_display'] ?? 'none';
 $isTemplate = $args['is_template'] ?? false;
 
-$postID = get_the_ID();
-$title = get_the_title();
-$excerpt = get_the_excerpt();
-$permalink = get_permalink();
-$authorName = get_the_author();
-$publishDate = get_the_date( 'F j, Y' );
-$featuredImageID = get_post_thumbnail_id() ?: PLACEHOLDER_IMAGE_ID;
-$categories = get_the_category();
+$postID = $isTemplate ? 0 : get_the_ID();
+$title = $isTemplate ? 'Post template' : get_the_title();
+$excerpt = $isTemplate ? 'Excerpt template' : get_the_excerpt();
+$permalink = $isTemplate ? '#' : get_permalink();
+$authorName = $isTemplate ? 'Author' : get_the_author_meta( 'display_name', get_post_field( 'post_author', $postID ) );
+$publishDate = $isTemplate ? '01/01/2025' : get_the_date( 'F j, Y', $postID );
+$featuredImageID = $isTemplate ? PLACEHOLDER_IMAGE_ID : get_post_thumbnail_id( $postID );
+$categories = get_the_category( $postID );
 $primaryCategory = get_post_meta( $postID, 'rank_math_primary_category', true );
 $primaryCategory = $primaryCategory 
   ? get_term( $primaryCategory ) 
-  : ( has_category() ? $categories[0] : null );
+  : ( has_category( '', $postID ) ? $categories[0] : null );
 
 $classes = [ 'post-card', "post-card--{$orientation}" ];
-$classes[] = $isTemplate ? 'post-card--template' : "post-card--{$postID}";
 
 if( $isFeatured ) {
   $classes[] = 'post-card--featured';
@@ -42,14 +41,15 @@ if( $isFeatured ) {
 } ?>
 <article class="<?= esc_attr( implode( ' ', $classes ) ) ?>" 
   <?php if( !$isTemplate ) echo sprintf('data-cat="%s"', esc_attr( implode( ',', wp_list_pluck( $categories, 'term_id' ) ) ) ) ?>
+  <?php if( !$isTemplate ) echo sprintf('data-id="%d"', esc_attr( $postID ) ) ?>
 >
   <a href="<?= esc_url( $permalink ) ?>" class="post-card__thumbnail">
     <?= $isTemplate ? '<img src="" alt="" class="post-card__image" />'
       : wp_get_attachment_image( $featuredImageID, 'medium_large', false, [ 'class' => 'post-card__image', 'alt' => esc_attr( $title ) ] ) ?>
   </a>
   <div class="post-card__content">
-    <?php if( $showCategory && $primaryCategory ) {
-      echo $isTemplate ? ''
+    <?php if( ($showCategory && $primaryCategory) || $isTemplate ) {
+      echo $isTemplate ? '<a href="#" class="post-card__category">Category</a>'
         : sprintf('<a href="%s" class="post-card__category">%s</a>',
             esc_url( get_category_link( $primaryCategory->term_id ) ),
             esc_html( $primaryCategory->name )
@@ -59,18 +59,18 @@ if( $isFeatured ) {
     <h3 class="post-card__title line-clamp">
 
       <?= sprintf('<a href="%s">%s</a>', 
-        $isTemplate ? '' : esc_url( $permalink ), 
-        $isTemplate ? '' : esc_html( $title ) ) ?>
+        esc_url( $permalink ), 
+        esc_html( $title ) ) ?>
 
     </h3>
     <?php if ( $showExcerpt && !empty( $excerpt ) ): ?>
       <div class="post-card__excerpt line-clamp">
-        <?= $isTemplate ? '' : esc_html( $excerpt ) ?>
+        <?= esc_html( $excerpt ) ?>
       </div>
     <?php endif; ?>
     <?php if( $footerDisplay === 'read-more' ): ?>
 
-      <a href="<?= $isTemplate ? '' : esc_url( $permalink ) ?>" class="post-card__read-more">
+      <a href="<?= esc_url( $permalink ) ?>" class="post-card__read-more">
         <span><?php esc_html_e( 'Read More', 'gpw' ); ?></span>
         <span class="material-symbols-outlined">arrow_right_alt</span>
       </a>
@@ -78,8 +78,8 @@ if( $isFeatured ) {
     <?php elseif( $footerDisplay === 'meta' ): ?>
 
       <ul class="post-card__meta">
-        <li class="post-card__meta-item author"><?= $isTemplate ? '' : esc_html( $authorName ) ?></li>
-        <li class="post-card__meta-item date"><?= $isTemplate ? '' : esc_html( $publishDate ) ?></li>
+        <li class="post-card__meta-item author"><?= esc_html( $authorName ) ?></li>
+        <li class="post-card__meta-item date"><?= esc_html( $publishDate ) ?></li>
       </ul>
 
     <?php endif ?>
