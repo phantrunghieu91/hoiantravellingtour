8260a2d301dea165dcad759e754e566c70a6813e
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-01-05 16:39:20 +0700

    UPDATE: Blog page

---
+++ home.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * @author Hieu "Jin" Phan Trung
+ * * Template: Post archive page
+ */
+get_template_part( 'gpw-templates/global/header' );
+
+get_template_part( 'gpw-templates/post/category/header' );
+
+get_template_part( 'gpw-templates/post/category/latest-posts-section' );
+
+get_template_part( 'gpw-templates/post/category/posts-grid-section' );
+
+get_template_part( 'gpw-templates/global/footer' );
