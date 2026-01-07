761460a1af0897cc37dee348fc043bba762cff3f
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-01-07 15:36:04 +0700

    UPDATE: Add hero section

--- archive-logistics-solution.php
+++ archive-logistics-solution.php
@@ -6,6 +6,8 @@
 
 get_template_part( 'gpw-templates/global/header' );
 
+get_template_part( 'gpw-templates/global/hero-section', 'with-content' );
+
 get_template_part( 'gpw-templates/global/services-section', null, ['title' => '3A Logistics Solution'] );
 
 get_template_part( 'gpw-templates/global/footer' );
