c4f0879d3f26dd9e7cb9b067c39ee81d46f972a5
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-01-07 12:02:39 +0700

    UPDATE: Archive page for logistics solution post type + script and style

---
+++ archive-logistics-solution.php
@@ -0,0 +1,11 @@
+<?php 
+/**
+ * @author Hieu "Jin" Phan Trung
+ * * Template: Logistics Solution archive page
+ */
+
+get_template_part( 'gpw-templates/global/header' );
+
+get_template_part( 'gpw-templates/global/services-section', null, ['title' => '3A Logistics Solution'] );
+
+get_template_part( 'gpw-templates/global/footer' );
---
+++ src/js/export/gpw-logistics-solution-archive-page.js
@@ -0,0 +1,4 @@
+import ServicesCarousel from "../components/services-carousel";
+document.addEventListener('DOMContentLoaded', function() {
+  new ServicesCarousel();
+});
