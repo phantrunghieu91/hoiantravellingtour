9c6c7eca18187460beeb5de60a17546803e25d81
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-01-08 15:45:27 +0700

    UPDATE: Script handle change image related to accordion item

--- src/js/export/gpw-services-page.js
+++ src/js/export/gpw-services-page.js
@@ -1,4 +1,23 @@
 import ServicesCarousel from "../components/gpw-services-carousel";
+import GPWAccordion from "../components/gpw-accordion";
 document.addEventListener('DOMContentLoaded', function() {
+  const gpwAccordion = new GPWAccordion();
+  if( gpwAccordion.accordions.length > 0 ) {
+    const expertiseImgs = [...document.querySelectorAll('.expertise__features-img')];
+    gpwAccordion.accordions.forEach(accordion => {
+      accordion.addEventListener('gpw_accordion:toggle', event => {
+        const { button } = event.detail;
+        let index = button.getAttribute('aria-controls');
+        index = index.split('-').pop();
+        expertiseImgs.forEach((img, imgIndex) => {
+          if( imgIndex == index ) {
+            img.setAttribute('aria-hidden', 'false');
+          } else {
+            img.setAttribute('aria-hidden', 'true');
+          }
+        });
+      });
+    });
+  } 
   new ServicesCarousel();
 });
