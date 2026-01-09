f316fb348083cd49af06f398d3bb6dad62ee9d4b
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-01-09 09:59:12 +0700

    UPDATE: Add script for suitable industry carousel

--- src/js/export/gpw-services-page.js
+++ src/js/export/gpw-services-page.js
@@ -19,5 +19,44 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     });
   } 
+  // suitable industry
+  const suitableIndustryCarousel = {
+    init() {
+      try {
+        this.cacheElements();
+        this.initSwiper();
+      } catch( error ) {
+        console.warn( 'SUITABLE INDUSTRY CAROUSEL:', error.message );
+      }
+    },
+    cacheElements() {
+      this.swiperEl = document.querySelector('.suitable-industry__carousel .swiper');
+      if( !this.swiperEl ) {
+        throw new Error('No suitable industry carousel found');
+      }
+    },
+    initSwiper() {
+      if( typeof Swiper === 'undefined' ) {
+        throw new Error('Swiper is not loaded');
+      }
+      this.swiper = new Swiper(this.swiperEl, {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        navigation: {
+          nextEl: '.suitable-industry__carousel .gpw-nav-btn__next',
+          prevEl: '.suitable-industry__carousel .gpw-nav-btn__prev',
+        },
+        breakpoints: {
+          550: {
+            slidesPerView: 2,
+          },
+          850: {
+            slidesPerView: 3,
+          }
+        }
+      });
+    }
+  }.init();
+
   new ServicesCarousel();
 });
