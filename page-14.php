55503b66a70a9fa09903d7509e5532263a07e60b
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-01-14 15:50:56 +0700

    CREATE: Contact page template, style + enqueue style for contact page

---
+++ page-14.php
@@ -0,0 +1,11 @@
+<?php 
+/**
+ * @author Hieu "Jin" Phan Trung
+ * * Template: Page contact
+ */
+
+get_template_part( 'gpw-templates/global/header' );
+
+get_template_part( 'gpw-templates/global/hero-section', 'with-content' );
+
+get_template_part( 'gpw-templates/global/footer' );
