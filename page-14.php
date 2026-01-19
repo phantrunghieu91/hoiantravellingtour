4bef5f3fc18737b663fa85873d28c55d3953e044
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-01-19 09:48:41 +0700

    FIXED: Contact page - Replace contact form section with get a free quote section

--- page-14.php
+++ page-14.php
@@ -10,7 +10,7 @@ get_template_part( 'gpw-templates/global/hero-section', 'with-content' );
 
 get_template_part( 'gpw-templates/contact-page/contact-info-section' );
 
-get_template_part( 'gpw-templates/contact-page/contact-form-section' );
+get_template_part( 'gpw-templates/global/get-free-quote-section' );
 
 get_template_part( 'gpw-templates/contact-page/connect-with-experts-section' );
 
