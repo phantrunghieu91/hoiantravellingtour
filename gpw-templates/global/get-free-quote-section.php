3cc04eeb37c235d6e439d4683b0ef7a7e1bb0ba0
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-01-19 09:47:44 +0700

    FIXED: Global get a free quote section changed form and style

--- gpw-templates/global/get-free-quote-section.php
+++ gpw-templates/global/get-free-quote-section.php
@@ -8,19 +8,22 @@ if( empty( $sectionData['title'] ) ) {
   error_log('GET A QUOTE SECTION: Title is required.');
   return;
 }
-$formSC = '[contact-form-7 id="4e0f5b0" title="GLOBAL: Get a free quote"]';
+// $formSC = '[contact-form-7 id="4e0f5b0" title="GLOBAL: Get a free quote"]';
+$formSC = '[contact-form-7 id="3149cb5" title="GLOBAL: Get a free quote (NEW)"]';
 $sectionBgUrl = !empty( $sectionData['background_image'] ) ? wp_get_attachment_image_url( $sectionData['background_image'], 'full' ) : false;
 ?>
 <section class="get-free-quote" <?php if( $sectionBgUrl ) echo sprintf('style="--_background-image:url(%s)"', esc_url($sectionBgUrl)); ?> >
   <div class="section__inner">
 
-    <?php if( !empty($sectionData[ 'sub_title' ])): ?>
-
-      <span class="section__sub-title section__sub-title--secondary"><?= esc_html( $sectionData['sub_title']) ?></span>
-
+    <?php if( !empty( $sectionData['sub_title'] )): ?>
+      <span class="section__sub-title section__sub-title--center"><?php esc_html_e( $sectionData['sub_title'] ); ?></span>
     <?php endif ?>
-
-    <h2 class="section__title"><?= esc_html( $sectionData['title']) ?></h2>
+    <?php if( !empty( $sectionData['title'] )): ?>
+      <h2 class="section__title section__title--center"><?php esc_html_e( $sectionData['title'] ); ?></h2>
+    <?php endif ?>
+    <?php if( !empty( $sectionData['description'] )): ?>
+      <div class="section__description section__description--center"><?php echo wp_kses_post( $sectionData['description'] ); ?></div>
+    <?php endif ?>
 
     <div class="get-free-quote__form">
       <?= do_shortcode( $formSC ) ?>
