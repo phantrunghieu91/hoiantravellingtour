0c9d4a6556ebca1b028129fb4ac83403341a3ede
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-01-23 08:51:13 +0700

    UPDATE: Archive logistics solution page - Fixed typo in testimonial style file - Added translation function for logistics solution title - Added "Get Free Quote" section + style - Added "Testimonial" section + style

--- archive-logistics-solution.php
+++ archive-logistics-solution.php
@@ -8,6 +8,10 @@ get_template_part( 'gpw-templates/global/header' );
 
 get_template_part( 'gpw-templates/global/hero-section', 'with-content' );
 
-get_template_part( 'gpw-templates/global/services-section', null, ['title' => '3A Logistics Solution'] );
+get_template_part( 'gpw-templates/global/services-section', null, ['title' => __('3A Logistics Solution', GPW_TEXT_DOMAIN)] );
+
+get_template_part( 'gpw-templates/global/get-free-quote-section' );
+
+get_template_part( 'gpw-templates/global/testimonial-section');
 
 get_template_part( 'gpw-templates/global/footer' );
