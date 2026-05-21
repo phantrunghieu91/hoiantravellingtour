cd945a3954323326ee05eee62addd280f0a635d4
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-05-21 09:59:57 +0700

    UPDATE: Home page - Change position of services-section and our-solution-section

--- page-home-template.php
+++ page-home-template.php
@@ -6,15 +6,15 @@
 
 get_template_part( 'gpw-templates/global/header' );
 
-get_template_part( 'gpw-templates/global/hero-section', 'with-content' );
+get_template_part( 'gpw-templates/global/hero-section', 'with-content', [ 'split_title' => 'true' ] );
 
-get_template_part( 'gpw-templates/home-page/our-solutions-section' );
+get_template_part( 'gpw-templates/global/services-section', null, [ 'has_view_all_button' => true ] );
 
 get_template_part( 'gpw-templates/home-page/core-value-section', 'style-2' );
 
 get_template_part( 'gpw-templates/home-page/statistic-section' );
 
-get_template_part( 'gpw-templates/global/services-section', null, [ 'has_view_all_button' => true ] );
+get_template_part( 'gpw-templates/home-page/our-solutions-section' );
 
 get_template_part( 'gpw-templates/global/get-free-quote-section' );
 
