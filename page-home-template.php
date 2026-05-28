ff21cefe1a4ea0a3ad849d1ae959c416ca1641bf
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-05-28 09:47:38 +0700

    UPDATE: Home page - Added case study section + change statistics section position

--- page-home-template.php
+++ page-home-template.php
@@ -10,11 +10,13 @@ get_template_part( 'gpw-templates/global/hero-section', 'with-content', [ 'split
 
 get_template_part( 'gpw-templates/global/services-section', null, [ 'has_view_all_button' => true ] );
 
-get_template_part( 'gpw-templates/home-page/core-value-section', 'style-2' );
+get_template_part( 'gpw-templates/home-page/our-solutions-section' );
+
+get_template_part( 'gpw-templates/global/case-studies-section' );
 
 get_template_part( 'gpw-templates/home-page/statistic-section' );
 
-get_template_part( 'gpw-templates/home-page/our-solutions-section' );
+get_template_part( 'gpw-templates/home-page/core-value-section', 'style-2' );
 
 get_template_part( 'gpw-templates/global/get-free-quote-section' );
 
