4edfb9d34c87c5c518c9c56de052d5c3cad2e491
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-05-28 15:49:36 +0700

    UPDATE: Home page

--- page-home-template.php
+++ page-home-template.php
@@ -20,9 +20,9 @@ get_template_part( 'gpw-templates/home-page/what-make-different-section' );
 
 get_template_part( 'gpw-templates/home-page/member-of-group-section' );
 
-get_template_part( 'gpw-templates/home-page/core-value-section', 'style-2' );
+get_template_part( 'gpw-templates/footer/partners-section' );
 
-get_template_part( 'gpw-templates/global/get-free-quote-section' );
+get_template_part( 'gpw-templates/home-page/destination-section' );
 
 get_template_part( 'gpw-templates/global/testimonial-section');
 
