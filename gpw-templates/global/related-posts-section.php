04a8411c2c0ff215d2fc963e0ffe00418c91954d
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-06-16 14:51:35 +0700

    UPDATE: Global - related posts section - Added render condition

--- gpw-templates/global/related-posts-section.php
+++ gpw-templates/global/related-posts-section.php
@@ -1,32 +1,35 @@
-<?php 
+<?php
 /**
  * @author Hieu "Jin" Phan Trung
  * * Template: Global - Related posts section
  */
 $sectionData = get_field( 'related_posts', get_the_ID() );
-if( empty( $sectionData['categories' ] ) ) {
-  do_action('qm/debug', 'Please select categories for related posts!');
+if ( empty( $sectionData['categories' ] ) ) {
+  do_action( 'qm/debug', 'Please select categories for related posts!' );
   return;
 }
 $defaultQuery = [
-  'post_type' => 'post',
+  'post_type'   => 'post',
   'numberposts' => 6,
   'post_status' => 'publish',
 ];
 $relatedPosts = [];
 $addedPostIds = [];
-foreach( $sectionData['categories'] as $catId ) {
+foreach ( $sectionData['categories'] as $catId ) {
   $query = array_merge( $defaultQuery, [ 'category' => $catId ] );
   $posts = get_posts( $query );
-  if( empty( $posts ) ) {
+  if ( empty( $posts ) ) {
     continue;
   }
-  foreach( $posts as $post ) {
-    if( !in_array( $post->ID, $addedPostIds ) ) {
+  foreach ( $posts as $post ) {
+    if ( !in_array( $post->ID, $addedPostIds ) ) {
       $relatedPosts[] = $post;
       $addedPostIds[] = $post->ID;
     }
   }
+}
+if ( empty( $relatedPosts ) ) {
+  return;
 }
 ?>
 <section class="related-posts">
@@ -36,20 +39,18 @@ foreach( $sectionData['categories'] as $catId ) {
       <ul class="related-posts__nav-list">
 
         <li class="related-posts__nav-item related-posts__nav-item--active" data-cat="0">
-          <?php _e('All', 'gpw') ?>
+          <?php _e( 'All', 'gpw' ) ?>
         </li>
 
-        <?php foreach ($sectionData['categories'] as $catID):
+        <?php foreach ( $sectionData['categories'] as $catID ):
           $category = get_category( $catID );
-          if( $category->count === 0 ) {
+          if ( $category->count === 0 ) {
             continue;
           }
-        ?>
+          ?>
 
-          <li class="related-posts__nav-item" data-cat="<?= esc_attr($catID) ?>">
-
-            <?= esc_html($category->name) ?>
-
+          <li class="related-posts__nav-item" data-cat="<?= esc_attr( $catID ) ?>">
+            <?= esc_html( $category->name ) ?>
           </li>
 
         <?php endforeach ?>
@@ -58,11 +59,11 @@ foreach( $sectionData['categories'] as $catId ) {
     </nav>
     <main class="related-posts__grid">
 
-      <?php foreach ($relatedPosts as $idx => $post) {
-        setup_postdata($post);
+      <?php foreach ( $relatedPosts as $idx => $post ) {
+        setup_postdata( $post );
         get_template_part( 'gpw-templates/post/post-card', null, [ 'show_category' => true, 'footer_display' => 'meta', 'is_hidden' => $idx > 5 ] );
-      } 
-      wp_reset_postdata(); ?>
+      } ?>
+      <?php wp_reset_postdata(); ?>
         
     </main>
     <a href="<?= get_permalink( get_option( 'page_for_posts' ) ) ?>" class="gpw-button gpw-button__primary gpw-button--center related-posts__view-all-btn" role="button">
