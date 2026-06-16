df2722d1f8036c112131c5a4fba4b3662f49e087
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-06-16 14:55:55 +0700

    UPDATE: Home page - Center image in member of group section

--- inc/base/Register.php
+++ inc/base/Register.php
@@ -6,184 +6,175 @@
 
 namespace gpweb\inc\base;
 
-class Register extends BaseController
-{
-    /**
-     ** An array of shortcodes.
-     * @var array
-     */
-    protected $shortcodes;
-    protected $module_scripts = [];
+class Register extends BaseController {
+  /**
+   ** An array of shortcodes.
+   * @var array
+   */
+  protected $shortcodes;
+  protected $module_scripts = [];
 
-    /**
-     * Registers the necessary actions and filters.
-     */
-    public function register()
-    {
-        add_action('wp_enqueue_scripts', [$this, 'enqueue']);
-        add_action('wp_enqueue_scripts', [$this, 'setTypeForModuleScripts']);
-        // Add AOS init script in the header
-        add_action('wp_footer', function () {
-            echo '<script> AOS.init(); </script>';
-        });
-        $this->setShortcodes();
-        add_action('init', [$this, 'registerShortcodes']);
+  /**
+   * Registers the necessary actions and filters.
+   */
+  public function register() {
+    add_action( 'wp_enqueue_scripts', [$this, 'enqueue'] );
+    add_action( 'wp_enqueue_scripts', [$this, 'setTypeForModuleScripts'] );
+    // Add AOS init script in the header
+    add_action( 'wp_footer', function () {
+      echo '<script> AOS.init(); </script>';
+    } );
+    $this->setShortcodes();
+    add_action( 'init', [$this, 'registerShortcodes'] );
 
-        // Add Facebook SDK
-        add_action('wp_head', [$this, 'addFacebookSDK']);
-    }
+    // Add Facebook SDK
+    add_action( 'wp_head', [$this, 'addFacebookSDK'] );
+  }
 
-    public function addFacebookSDK()
-    {
-        ?>
+  public function addFacebookSDK() {
+    ?>
     <div id="fb-root"></div>
     <script id="fb-root-script" async defer crossorigin="anonymous"
       src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v24.0&appId=APP_ID"></script>
     <?php
+  }
+
+  /**
+   * Sets the shortcodes.
+   */
+  protected function setShortcodes() {
+    $this->shortcodes = [
+      new \gpweb\shortcodes\ImgById( 'img_by_id' ),
+      new \gpweb\shortcodes\LinkTo( 'link_to' ),
+      new \gpweb\shortcodes\CertificateList( 'certificate_list_sc' ),
+      new \gpweb\shortcodes\LanguageSwitcher( 'gpw_language_switcher' ),
+      new \gpweb\shortcodes\HeaderHotline( 'gpw_header_hotline' ),
+    ];
+  }
+
+  /**
+   * Enqueues the necessary scripts and styles.
+   */
+  public function enqueue() {
+    $this->enqueueScript( 'aos', null, false, '', [], false );
+    $this->enqueueStyle( 'aos', null );
+
+    $this->enqueueStyle( 'theme-init', '1.0.8' );
+    $this->enqueueStyle( 'google-symbols', null, 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200' );
+
+    $this->enqueueStyle( 'gpw-header', '1.0.3' );
+    $this->enqueueStyle( 'gpw-footer', '1.0.3' );
+
+    // * Enqueue swiper for page that needs it
+    if ( is_front_page() || is_post_type_archive( ['logistics-solution', 'industry'] ) || is_singular( 'logistics-solution' ) || is_singular( 'industry' ) || is_page( [ 508, 13, 2695 ] ) ) {
+      $this->enqueueScript( 'swiper' );
+      $this->enqueueStyle( 'swiper' );
     }
 
-    /**
-     * Sets the shortcodes.
-     */
-    protected function setShortcodes()
-    {
-        $this->shortcodes = [
-          new \gpweb\shortcodes\ImgById('img_by_id'),
-          new \gpweb\shortcodes\LinkTo('link_to'),
-          new \gpweb\shortcodes\CertificateList('certificate_list_sc'),
-          new \gpweb\shortcodes\LanguageSwitcher('gpw_language_switcher'),
-          new \gpweb\shortcodes\HeaderHotline('gpw_header_hotline'),
-        ];
+    if ( is_front_page() ) {
+      $this->enqueueScript( 'gpw-home-page', '1.0.6', true );
+      $this->enqueueStyle( 'gpw-home-page', '1.0.16' );
     }
 
-    /**
-     * Enqueues the necessary scripts and styles.
-     */
-    public function enqueue()
-    {
-        $this->enqueueScript('aos', null, false, '', [], false);
-        $this->enqueueStyle('aos', null);
+    if ( is_home() || is_category() ) {
+      $postController = \gpweb\inc\controller\PostController::getInstance();
+      $action         = $postController->getAction();
+      $this->enqueueScript( 'gpw-post-category-page', '1.0.0' );
+      $this->enqueueStyle( 'gpw-post-category-page', '1.0.0' );
+      wp_localize_script( 'gpw-post-category-page', 'gpwObject', [
+        'url'    => admin_url( 'admin-ajax.php' ),
+        'action' => $action,
+        'nonce'  => wp_create_nonce( $action ),
+      ] );
 
-        $this->enqueueStyle('theme-init', '1.0.8');
-        $this->enqueueStyle('google-symbols', null, 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200');
-
-        $this->enqueueStyle('gpw-header', '1.0.3');
-        $this->enqueueStyle('gpw-footer', '1.0.3');
-
-        // * Enqueue swiper for page that needs it
-        if (is_front_page() || is_post_type_archive(['logistics-solution', 'industry']) || is_singular('logistics-solution') || is_singular('industry') || is_page([ 508, 13, 2695 ])) {
-            $this->enqueueScript('swiper');
-            $this->enqueueStyle('swiper');
-        }
-
-        if (is_front_page()) {
-            $this->enqueueScript('gpw-home-page', '1.0.6', true);
-            $this->enqueueStyle('gpw-home-page', '1.0.15');
-        }
-
-        if (is_home() || is_category()) {
-            $postController = \gpweb\inc\controller\PostController::getInstance();
-            $action = $postController->getAction();
-            $this->enqueueScript('gpw-post-category-page', '1.0.0');
-            $this->enqueueStyle('gpw-post-category-page', '1.0.0');
-            wp_localize_script('gpw-post-category-page', 'gpwObject', [
-              'url' => admin_url('admin-ajax.php'),
-              'action' => $action,
-              'nonce' => wp_create_nonce($action),
-            ]);
-
-            unset($action, $postController);
-        }
-
-        if (is_singular('post')) {
-            $this->enqueueStyle('gpw-post-single-page', '1.0.0');
-        }
-
-        if (is_post_type_archive(['logistics-solution', 'industry']) || is_singular('logistics-solution')) {
-            $this->enqueueScript('gpw-services-page', '1.0.2', true);
-            $this->enqueueStyle('gpw-services-page', '1.0.14');
-        }
-
-        if (is_singular('industry')) {
-            $this->enqueueScript('gpw-industry-single-page', '1.0.4', true);
-            $this->enqueueStyle('gpw-industry-single-page', '1.0.10');
-        }
-
-        if (is_singular('career')) {
-            $this->enqueueStyle('gpw-career-single-page', '1.0.3');
-        }
-
-        if (is_page([14, 2313])) {
-            $this->enqueueStyle('gpw-contact-page', '1.0.9');
-        }
-
-        if (is_page([13, 2695])) {
-            $this->enqueueScript('gpw-about-page', '1.0.4', true);
-            $this->enqueueStyle('gpw-about-page', '1.0.9');
-        }
-
-        if (is_page([508,2310])) {
-            $action = \gpweb\inc\controller\CareerController::getInstance()->getAction();
-
-            $this->enqueueScript('gpw-careers-page', '1.0.3', true);
-            $this->enqueueStyle('gpw-careers-page', '1.0.8');
-            wp_localize_script('gpw-careers-page', 'ajaxObj', [
-              'url' => admin_url('admin-ajax.php'),
-              'action' => $action,
-              'nonce' => wp_create_nonce($action),
-            ]);
-        }
-
-        if (is_page([1621, 2305])) {
-            $this->enqueueStyle('gpw-be-our-agent-page', '1.0.2');
-        }
-
-        if (is_page([1671, 2308])) {
-            $this->enqueueStyle('gpw-sustainability-page', '1.0.2');
-        }
-    }
-    public function setTypeForModuleScripts()
-    {
-        if (empty($this->module_scripts)) {
-            return;
-        }
-        add_filter('script_loader_tag', function ($tag, $handle, $src) {
-            if (in_array($handle, $this->module_scripts)) {
-                $tag = '<script type="module" src="' . esc_url($src) . '"></script>';
-            }
-            return $tag;
-        }, 10, 3);
+      unset( $action, $postController );
     }
 
-    /**
-     ** Enqueue single script
-     */
-    protected function enqueueScript(string $script_name, ?string $version = null, bool $is_module = false, string $url = '', array $dependencies = [], bool $in_footer = true)
-    {
-        if ($is_module) {
-            $this->module_scripts[] = $script_name;
-        }
-        $url = $url === '' ? "{$this->theme_url}/assets/js/{$script_name}.min.js" : $url;
-        wp_enqueue_script($script_name, $url, $dependencies, $version, $in_footer);
+    if ( is_singular( 'post' ) ) {
+      $this->enqueueStyle( 'gpw-post-single-page', '1.0.0' );
     }
 
-    /**
-     ** Enqueue single style
-     */
-    public function enqueueStyle(string $style_name, ?string $version = null, string $url = '', array $dependencies = [], string $media = 'all')
-    {
-        $url = $url === '' ? "{$this->theme_url}/assets/css/{$style_name}.min.css" : $url;
-        wp_enqueue_style($style_name, $url, $dependencies, $version, $media);
+    if ( is_post_type_archive( ['logistics-solution', 'industry'] ) || is_singular( 'logistics-solution' ) ) {
+      $this->enqueueScript( 'gpw-services-page', '1.0.2', true );
+      $this->enqueueStyle( 'gpw-services-page', '1.0.14' );
     }
 
-    /**
-     * * Registers the shortcodes.
-     * * Loops through the shortcodes array and registers each shortcode.
-     */
-    public function registerShortcodes()
-    {
-        foreach ($this->shortcodes as $shortcode) {
-            add_shortcode($shortcode->getShortcodeName(), [$shortcode, 'shortcodeCallback']);
-        }
+    if ( is_singular( 'industry' ) ) {
+      $this->enqueueScript( 'gpw-industry-single-page', '1.0.4', true );
+      $this->enqueueStyle( 'gpw-industry-single-page', '1.0.10' );
     }
+
+    if ( is_singular( 'career' ) ) {
+      $this->enqueueStyle( 'gpw-career-single-page', '1.0.3' );
+    }
+
+    if ( is_page( [14, 2313] ) ) {
+      $this->enqueueStyle( 'gpw-contact-page', '1.0.9' );
+    }
+
+    if ( is_page( [13, 2695] ) ) {
+      $this->enqueueScript( 'gpw-about-page', '1.0.4', true );
+      $this->enqueueStyle( 'gpw-about-page', '1.0.9' );
+    }
+
+    if ( is_page( [508, 2310] ) ) {
+      $action = \gpweb\inc\controller\CareerController::getInstance()->getAction();
+
+      $this->enqueueScript( 'gpw-careers-page', '1.0.3', true );
+      $this->enqueueStyle( 'gpw-careers-page', '1.0.8' );
+      wp_localize_script( 'gpw-careers-page', 'ajaxObj', [
+        'url'    => admin_url( 'admin-ajax.php' ),
+        'action' => $action,
+        'nonce'  => wp_create_nonce( $action ),
+      ] );
+    }
+
+    if ( is_page( [1621, 2305] ) ) {
+      $this->enqueueStyle( 'gpw-be-our-agent-page', '1.0.2' );
+    }
+
+    if ( is_page( [1671, 2308] ) ) {
+      $this->enqueueStyle( 'gpw-sustainability-page', '1.0.2' );
+    }
+  }
+  public function setTypeForModuleScripts() {
+    if ( empty( $this->module_scripts ) ) {
+      return;
+    }
+    add_filter( 'script_loader_tag', function ( $tag, $handle, $src ) {
+      if ( in_array( $handle, $this->module_scripts ) ) {
+        $tag = '<script type="module" src="' . esc_url( $src ) . '"></script>';
+      }
+      return $tag;
+    }, 10, 3 );
+  }
+
+  /**
+   ** Enqueue single script
+   */
+  protected function enqueueScript( string $script_name, ?string $version = null, bool $is_module = false, string $url = '', array $dependencies = [], bool $in_footer = true ) {
+    if ( $is_module ) {
+      $this->module_scripts[] = $script_name;
+    }
+    $url = $url === '' ? "{$this->theme_url}/assets/js/{$script_name}.min.js" : $url;
+    wp_enqueue_script( $script_name, $url, $dependencies, $version, $in_footer );
+  }
+
+  /**
+   ** Enqueue single style
+   */
+  public function enqueueStyle( string $style_name, ?string $version = null, string $url = '', array $dependencies = [], string $media = 'all' ) {
+    $url = $url === '' ? "{$this->theme_url}/assets/css/{$style_name}.min.css" : $url;
+    wp_enqueue_style( $style_name, $url, $dependencies, $version, $media );
+  }
+
+  /**
+   * * Registers the shortcodes.
+   * * Loops through the shortcodes array and registers each shortcode.
+   */
+  public function registerShortcodes() {
+    foreach ( $this->shortcodes as $shortcode ) {
+      add_shortcode( $shortcode->getShortcodeName(), [$shortcode, 'shortcodeCallback'] );
+    }
+  }
 }
