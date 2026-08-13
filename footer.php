89922f60c2c146ff1036e68f11b77c487732646f
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-08-13 10:25:48 +0700

    FIXED: Footer - render background image

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
  * @author Hieu "Jin" Phan Trung
  * * The template for displaying the footer.
  */
-$footerBgID = get_field('footer_background_image', 'gpw_settings');
+$footerBgID = get_field( 'footer_background_image', 'gpw_settings' );
 ?>
 
 <?php get_template_part( 'gpw-templates/footer/consultant-section' ) ?>
@@ -11,7 +11,7 @@ $footerBgID = get_field('footer_background_image', 'gpw_settings');
 </main>
 
 <footer id="footer" class="footer" 
-	<?php if ($footerBgID) : ?> style="background-image: url('<?php echo esc_url(wp_get_attachment_url($footerBgID), 'full'); ?>');" <?php endif; ?>
+	<?php if ( $footerBgID ) : ?> style="background-image: url('<?php echo esc_url( wp_get_attachment_image_url( $footerBgID, 'full' ) ); ?>');" <?php endif; ?>
 >
 
 
@@ -22,10 +22,10 @@ $footerBgID = get_field('footer_background_image', 'gpw_settings');
 	<?php get_template_part( 'gpw-templates/footer/bottom-section' ) ?>
 
 	<?php
-	if (get_theme_mod('back_to_top', 1)) {
-		get_template_part('template-parts/footer/back-to-top');
-	}
-	?>
+    if ( get_theme_mod( 'back_to_top', 1 ) ) {
+      get_template_part( 'template-parts/footer/back-to-top' );
+    }
+?>
 
 </footer>
 
